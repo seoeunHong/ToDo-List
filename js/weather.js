330ef6cde4bff7e9c1175dc2dc23865fb026ecd9
Seoeun Hong <sh6348@nyu.edu>
2022-04-10 03:20:50 -0400

    Modification

--- js/weather.js
+++ js/weather.js
@@ -1,9 +1,9 @@
-const API_KEY = "0c4d8daaf167c4f224b9b6b29ff18f66";
+//const API_KEY = "0c4d8daaf167c4f224b9b6b29ff18f66";
 
 function onGeoSuccess(position) {
   const lat = position.coords.latitude;
   const lon = position.coords.longitude;
-  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${config.API_KEY}&units=metric`;
   fetch(url).then((response) =>
     response.json().then((data) => {
       const weather = document.querySelector("#weather span:first-child");
